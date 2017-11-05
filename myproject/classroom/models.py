57a962c958116f11d134fc838b3456b787e4430f
Kimbbakar <chowdhuryosman04@gmail.com>
2017-11-05 22:40:32 +0600

    create classroo_lecture model

--- myproject/classroom/models.py
+++ myproject/classroom/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class lecture(models.Model):
+#	course_id =  TODO
+	lecture_id =  models.AutoField(primary_key=True)
+	slid_link = models.CharField(max_length=20)
+	description = models.CharField(max_length=400,null=True)
