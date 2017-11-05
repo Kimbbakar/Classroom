06377a21f87271aaa6ff3fc96c86173855fc8d24
Kimbbakar <chowdhuryosman04@gmail.com>
2017-11-05 23:12:05 +0600

    update classroom_lecture model

--- myproject/classroom/models.py
+++ myproject/classroom/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class lecture(models.Model):
-#	course_id =  TODO
-	lecture_id =  models.AutoField(primary_key=True)
-	slid_link = models.CharField(max_length=20)
-	description = models.CharField(max_length=400,null=True)
+#   course_id =  TODO
+
+    name = models.CharField(max_length=30, unique=True)
+    lecture_id =  models.AutoField(primary_key=True)
+    slid_link = models.CharField(max_length=20)
+    description = models.CharField(max_length=400,null=True)
