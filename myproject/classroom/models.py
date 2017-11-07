14bc0a8f3bb4c73a80e3acfb0574f2315c9dd050
Kimbbakar <chowdhuryosman04@gmail.com>
2017-11-07 23:22:15 +0600

    lecture model has been updated

--- myproject/classroom/models.py
+++ myproject/classroom/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
+
 # Create your models here.
 
 class lecture(models.Model):
 #   course_id =  TODO
 
     name = models.CharField(max_length=30, unique=True)
-    lecture_id =  models.AutoField(primary_key=True)
-    slid_link = models.CharField(max_length=20)
+    lecture_no =  models.IntegerField ()
+    link = models.CharField(max_length=20)
     description = models.CharField(max_length=400,null=True)
+    date = models.DateField(default=date.today)
