cf2843f0746a05d7245f60cff9c6965815bcdb69
Kimbbakar <chowdhuryosman04@gmail.com>
2017-11-07 23:27:46 +0600

    add home page with lecture list

--- myproject/classroom/views.py
+++ myproject/classroom/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import lecture
 
 # Create your views here.
+
+def home(request):
+    lectures = lecture.objects.all()
+
+    return render(request,'home.html',{'lectures':lectures } ) 
