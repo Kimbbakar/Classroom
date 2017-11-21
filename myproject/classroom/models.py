d51b291ca721ff91cb394d440e4f3fd5daeeed18
Kimbbakar <chowdhuryosman04@gmail.com>
2017-11-22 01:09:06 +0600

    model add: user_type,registration

--- myproject/classroom/models.py
+++ myproject/classroom/models.py
@@ -8,9 +8,16 @@ class course(models.Model):
     course_id = models.CharField(max_length=10,  primary_key=True)
     course_name = models.CharField(max_length=30)
     faculty = models.CharField(max_length=10)
-    semester = models.CharField(max_length=10)
- 
+    semester = models.ForeignKey(User, related_name='faculty', default= 'Admin' )
 
+
+class user_type(models.Model):
+    user = models.ForeignKey(User,related_name = 'type' )
+    role = models.IntegerField()
+
+class registration(models.Model):
+    user = models.ForeignKey(User,related_name = 'registration' )
+    course = models.ForeignKey(course, related_name='registration')
 
 class lecture(models.Model): 	
     course = models.ForeignKey(course, related_name='lectures', default= 'Admin' )
