aea107f92d991a3dfaa0d47a892381b5232a6b91
Kimbbakar <chowdhuryosman04@gmail.com>
2017-11-21 23:11:33 +0600

    add first name and last name for sign up

--- myproject/accounts/forms.py
+++ myproject/accounts/forms.py
@@ -13,7 +13,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2','type')
+        fields = ('first_name','last_name', 'username', 'email', 'password1', 'password2','type')
 
 
     def clean_username(self):
